31fc856856e8a548ebd0346747c1aec615f3aa6a
조법룡 <jby0711823@naver.com>
2018-12-31 23:08:54 +0900

    다른 사람의 풀이 추가

--- algorithms/src/baekjoon/Q1977.java
+++ algorithms/src/baekjoon/Q1977.java
@@ -1,5 +1,11 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 // https://www.acmicpc.net/problem/1977
 
 import java.util.Scanner;
@@ -38,4 +44,35 @@ public class Q1977 {
 		
 		sc.close();
 	}
+
+	// 다른 사람의 풀이 - 참고 및 비교해 볼 것 
+	//	public static void main(String args[]) throws IOException {
+	//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	//
+	//		int M = Integer.parseInt(br.readLine());
+	//		int N = Integer.parseInt(br.readLine());
+	//
+	//		int x = 25;
+	//		int y = 25;
+	//		for (int i = 0; i < 5; i++) {
+	//			x = (x * x + M) / (2 * x);
+	//			y = (y * y + N) / (2 * y);
+	//		}
+	//		if (x * x < M) {
+	//			x++;
+	//		}
+	//
+	//		int sum = 0;
+	//		for (int i = x; i <= y; i++) {
+	//			sum += i * i;
+	//		}
+	//		if (sum == 0)
+	//			bw.write("-1\n");
+	//		else {
+	//			bw.write(sum + "\n");
+	//			bw.write(x * x + "\n");
+	//		}
+	//		bw.flush();
+	//	}
 }
