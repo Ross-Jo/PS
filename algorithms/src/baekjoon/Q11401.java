25ab21bc309150b2ac090fa02edb6d803c0cc9ab
조법룡 <jby0711823@naver.com>
2019-01-04 13:30:24 +0900

    이항계수 관련 문제

--- algorithms/src/baekjoon/Q11401.java
+++ algorithms/src/baekjoon/Q11401.java
@@ -1,33 +1,46 @@
 package baekjoon;
 
 // https://www.acmicpc.net/problem/11401
-// 오답 - 다시풀 것
+// 참고 : http://jason9319.tistory.com/169
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q11401 {
 	static final int MOD = 1000000007;
 	static int N, K;
-	static int[][] bino;
-	
-	static int calc(int n, int k) {
-		if (k==0 || n==k) return 1;
-		
-		int b = bino[n][k];
-		if (b != -1) return bino[n][k];
-		
-		return bino[n][k] = (calc(n-1, k-1) + calc(n-1, k)) % MOD;
+	static long[] fac = new long[4000001];
+	static long[] inv = new long[4000001]; // inv[x]의 정의는 x의 inverse가 아닌 x!의 inverse
+	static long power(long x, long y) {
+		long ret = 1;
+		while(y>0) {
+			if(y%2 == 1) {
+				ret *= x;
+				ret %= MOD;
+			}
+			x *= x;
+			x %= MOD;
+			y /= 2;
+		}
+		return ret;
 	}
-	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-		bino = new int[N+1][K+1];
-		for (int i=0; i<bino.length; i++) Arrays.fill(bino[i], -1);
 		
-		System.out.println(calc(N, K));
+		fac[0] = fac[1] = 1;
+		for (int i=1; i<=4000000; i++) fac[i] = (fac[i-1] * i) % MOD;
+		inv[4000000] = power(fac[4000000], MOD-2); // 페르마 소정리 
+		for (int i=4000000-1; i>=0; i--) inv[i] = (inv[i+1]*(i+1)) % MOD;
+		
+		if (K == 0 || N == K) {
+			System.out.println(1);
+		} else {
+			long r = (fac[N] * inv[N-K]) % MOD;
+			r = (r*inv[K]) % MOD;
+			
+			System.out.println(r);
+		}
 		
 		sc.close();
 	}
