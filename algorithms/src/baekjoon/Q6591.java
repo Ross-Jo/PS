344ed7c4075e25dea886b7aef58ab9c3ba6b6071
조법룡 <jby0711823@naver.com>
2019-01-24 15:48:20 +0900

    이항 계수 관련 문제

--- algorithms/src/baekjoon/Q6591.java
+++ algorithms/src/baekjoon/Q6591.java
@@ -1,29 +1,39 @@
 package baekjoon;
 
-// https://www.acmicpc.net/problem/6591
+//https://www.acmicpc.net/problem/6591
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Q6591 {
 	
 	static long calc(int n, int m) {
 		long ret = 1;
+		if (n==m) return 1;
 		if (m > n-m) m = n-m;
 		for (int i=1; i<=m; i++) ret = ret * (n-m+i) / i;
 		return ret;
 	}
 	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		while(true) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
+			String[] tmp = br.readLine().split(" ");
+			int n = Integer.parseInt(tmp[0]);
+			int m = Integer.parseInt(tmp[1]);
 			if (n==0 && m==0) break;
 			
-			System.out.println(calc(n, m));
+			bw.write(Long.toString(calc(n, m)));
+			bw.newLine();
 		}
 		
-		sc.close();
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
