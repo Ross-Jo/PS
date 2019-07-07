eca810d90cded37ef04b4b35a8f5821aaf00577e
조법룡 <jby0711823@naver.com>
2019-07-07 20:45:32 +0900

    linkedList

--- algorithms/src/leetCode/AddTwoNumbers.java
+++ algorithms/src/leetCode/AddTwoNumbers.java
@@ -7,7 +7,7 @@ public class AddTwoNumbers {
 		ListNode(int x) { val = x; }
 	}
 	
-	static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	static ListNode addTwoNumbers_mySol(ListNode l1, ListNode l2) {
 		ListNode now, tail = null, trailer = null;
 		int carry = 0;
 		boolean make = false, first = true;
@@ -57,6 +57,27 @@ public class AddTwoNumbers {
 		return tail;
 	}
 	
+	static ListNode addTwoNumbers_siteSol(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		
+		while (p != null || q != null ) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
+		} 
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+	}
+	
 	public static void main(String[] args) {
 		ListNode ln1 = new ListNode(2);
 		ln1.next = new ListNode(4);
@@ -66,7 +87,7 @@ public class AddTwoNumbers {
 		ln2.next = new ListNode(6);
 		ln2.next.next = new ListNode(4);
 		
-		ListNode ans = addTwoNumbers(ln1, ln2);
+		ListNode ans = addTwoNumbers_mySol(ln1, ln2);
 		do {
 			System.out.print(ans.val + " ");
 			ans = ans.next;
