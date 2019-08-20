102e3e265455a35085a6c8fcc0ee28c1b1231e39
조법룡 <jby0711823@naver.com>
2019-08-20 23:37:16 +0900

    -

--- algorithms/src/leetCode/LongestSubstringWithoutRepeatingCharacters.java
+++ algorithms/src/leetCode/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,18 +6,30 @@ import java.util.Map;
 // 솔루션 참고 
 
 public class LongestSubstringWithoutRepeatingCharacters {
+//	static int lengthOfLongestSubstring(String s) {
+//		int n = s.length(), ans = 0;
+//		Map<Character, Integer> map = new HashMap<>();
+//		for (int j = 0, i = 0; j < n; j++) {
+//			if (map.containsKey(s.charAt(j))) {
+//				i = Math.max(map.get(s.charAt(j)), i);
+//			}
+//			ans = Math.max(ans, j - i + 1);
+//			map.put(s.charAt(j), j + 1);
+//		}
+//		return ans;
+//	}
+	
 	static int lengthOfLongestSubstring(String s) {
 		int n = s.length(), ans = 0;
-		Map<Character, Integer> map = new HashMap<>();
+		int[] index = new int[128];
 		for (int j = 0, i = 0; j < n; j++) {
-			if (map.containsKey(s.charAt(j))) {
-				i = Math.max(map.get(s.charAt(j)), i);
-			}
+			i = Math.max(index[s.charAt(j)], i);
 			ans = Math.max(ans, j - i + 1);
-			map.put(s.charAt(j), j + 1);
+			index[s.charAt(j)] = j + 1;
 		}
 		return ans;
 	}
+	
 	public static void main(String args[]) {
 		System.out.println(lengthOfLongestSubstring("abcaefghi"));
 	}
